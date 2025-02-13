77b8012f105f1a8565b8122ae1968f89b78fc4af
g-falast14 <144640610+g-falast14@users.noreply.github.com>
2025-02-12 20:57:09 -0600

    added ability to drag and drop player objects to containers

--- static/js/game.js
+++ static/js/game.js
@@ -48,6 +48,53 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log("Fetching new player...");
         loadNewPlayer();
         console.log("Successfully grabbed new player");
-    })
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    let columns = document.querySelectorAll(".col");
+    let headshot = document.getElementById("player-image");
+
+    // Make headshot draggable
+    headshot.addEventListener("dragstart", function(event) {
+        const clonedImage = event.target.cloneNode(true);
+        clonedImage.id = "cloned-headshot";
+        event.dataTransfer.setData("text", event.target.id);
+        event.dataTransfer.setDragImage(clonedImage, 0, 0);
+    });
+
+    // Allow columns to accept drops
+    columns.forEach(column => {
+        column.addEventListener("dragover", event => event.preventDefault()); // allow drop
+        column.addEventListener("drop", function(event){
+
+            event.preventDefault();
+            const draggedElementId = event.dataTransfer.getData("text");
+            const draggedElement = document.getElementById(draggedElementId);
+
+            // create custom object
+            const customObject = document.createElement("div");
+            customObject.classList.add("custom-object");
+
+            // clone headshot image
+            const headshot = draggedElement.cloneNode(true);
+            headshot.style.width = "100px";
+            headshot.style.height = "100px";
+            headshot.style.marginBottom = "10px";
+
+            // player name
+            const playerName = "playername";
+            const label = document.createElement("p");
+            label.textContent = playerName;
+
+            // add elements to object
+            customObject.appendChild(headshot);
+            customObject.appendChild(label);
+
+            let container = column.querySelector('.fwd-object-container');
+            container.appendChild(customObject);
+
+        });
+    });
 
 });
